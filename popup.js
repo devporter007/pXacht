66519b633ab28a15e5fe490de0243c2d3b6feab3
devporter007 <portzero7@protonmail.com>
2021-10-20 19:56:10 +0530

    improved styling and url support

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 var sss = "";
-
+var currentUrl = "";
 chrome.storage.sync.get('lastUrl', function(data) {
     let url = "";
     if (data.lastUrl) url = data.lastUrl;
@@ -7,11 +7,30 @@ chrome.storage.sync.get('lastUrl', function(data) {
     
 });
 
-const btn = document.createElement('button');
-btn.innerText = 'RIP';
-document.body.appendChild(btn);
-btn.addEventListener('click',()=>{
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let url = tabs[0].url;
+    currentUrl = url;
+});
+const btnv = document.createElement('button');
+btnv.className = 'mpdButton';
+btnv.innerText = 'MPD';
+var divee = document.getElementById("divc1");
+divee.appendChild(btnv);
+btnv.classList.add('button');
+btnv.addEventListener('click',()=>{
 var fixedURI = sss.replace("https://","");
+window.open(`pxacht://${fixedURI}`,'_self')});
+
+
+
+const btn = document.createElement('button');
+btn.innerText = 'URL';
+btn.className = 'urlButton';
+var divee = document.getElementById("divc1");
+divee.appendChild(btn);
+btn.classList.add('button');
+btn.addEventListener('click',()=>{
+var fixedURI = currentUrl.replace("https://","");
 window.open(`pxacht://${fixedURI}`,'_self')
 });
 
